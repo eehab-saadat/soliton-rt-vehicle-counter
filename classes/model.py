2ae84bb98b027dee12f861b447111b7412978f1e
Muhammad Eehab Saadat <eehab.saadat@gmail.com>
2024-07-30 19:25:25 +0500

    added counter and csv writer

--- classes/model.py
+++ classes/model.py
@@ -1,5 +1,9 @@
-from ultralytics import YOLO
-from cv2 import imshow, waitKey, destroyAllWindows
+from copy import deepcopy
+from csv import writer
+from ultralytics import YOLO, solutions
+from ultralytics.solutions import ObjectCounter
+from cv2 import imshow, waitKey, destroyAllWindows, VideoCapture
+from cv2 import CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_POS_FRAMES
 
 class MODEL:
     __model = None
@@ -30,5 +34,71 @@ class MODEL:
                         break
             except Exception as e:
                 print(f"Error occurred: {e}")
-
             destroyAllWindows()
+
+    def __dump(self, counts: dict) -> None:
+        classes = self.__model.names
+        with open("counts.csv", "w", newline="") as file:
+            csv_writer = writer(file)
+            csv_writer.writerow(["class", "counts"])
+            for cls in list(classes.values()):
+                if cls in list(counts.keys()):
+                    csv_writer.writerow([cls, counts[cls]["IN"]])
+                else:
+                    csv_writer.writerow([cls, 0])
+
+    def __clean_count(self, counts: dict) -> None:
+        print(f"\nRAW = {counts}")
+        print("Object Counts:")
+        for cls, val in counts.items():
+            if isinstance(val, dict):
+                keys = list(val.keys())
+                if "IN" in keys and "OUT" in keys:
+                    for direction in keys:
+                        print(f"{cls.capitalize()} ({direction}) = {val[direction]}")
+
+    def count(self, source: str = "0", skip: int = 1, region_points: list = [], resolution: tuple = (1280, 720)) -> int:
+        if isinstance(self.__model, YOLO):
+            try:
+                frame_width, frame_height = resolution
+                frame_count = 0
+                source = source if source != "0" else 0
+                capture = VideoCapture(source)
+                capture.set(CAP_PROP_FRAME_WIDTH, frame_width)
+                capture.set(CAP_PROP_FRAME_HEIGHT, frame_height)
+                
+                object_counter = ObjectCounter(
+                    view_img=True,
+                    reg_pts=region_points,
+                    classes_names=self.__model.names,
+                    draw_tracks=True,
+                    line_thickness=2
+                )
+
+                new_classwise_count = {}
+                old_classwise_count = {}
+
+                while capture.isOpened():
+                    success, im0 = capture.read()
+                    if not success:
+                        break
+                    tracks = self.__model.track(im0, persist=True, show=False, verbose=False)
+                    im0 = object_counter.start_counting(im0, tracks)
+
+                    new_classwise_count = object_counter.class_wise_count
+
+                    if new_classwise_count != old_classwise_count:
+                        self.__clean_count(new_classwise_count)
+                        self.__dump(new_classwise_count)
+                        old_classwise_count = deepcopy(new_classwise_count)
+
+                    frame_count += skip
+                    capture.set(CAP_PROP_POS_FRAMES, frame_count)
+                    if waitKey(1) == ord('q'):
+                        break
+
+                capture.release()
+                destroyAllWindows()
+                
+            except Exception as e:
+                print(f"Error occurred: {e}")
--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from classes.model import MODEL
 from ultralytics import YOLO
 model = MODEL()
 model.mount(YOLO('models/final.pt'))
-model.execute(skip=3)
+region_points = [(500, 450), (550, 450), (550, 900), (500, 900)]
+linear_points = [(500, 450), (500, 900)]
+model.count("samples/sample.mp4", 1, linear_points)
+# mydict = {"bike":{"IN":1, "OUT":2}, "car":{"IN":5, "OUT":5}}
+# model.execute(skip=10)q
