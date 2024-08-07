3a3392e7e1b6ce52fac2a25907fc65b0e8af23b2
eehab-saadat <eehab.saadat@gmail.com>
2024-08-07 16:13:57 +0500

    added multi-threaded inference and timestamped csv writing

--- classes/model.py
+++ classes/model.py
@@ -3,25 +3,39 @@ from csv import writer
 from ultralytics import YOLO, solutions
 from ultralytics.solutions import ObjectCounter
 from cv2 import imshow, waitKey, destroyAllWindows, VideoCapture
-from cv2 import CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_POS_FRAMES
+from cv2 import CAP_PROP_POS_FRAMES
 from cv2.typing import MatLike
-import streamlit as st
+from datetime import datetime 
+from datetime import datetime
+def getName(source: str):
+    if isinstance(source, int):
+        source = str(source)
 
-@st.fragment(run_every=0.05)
-def updateFrame(frame: MatLike):
-    st.session_state.frame_bucket.image(frame, channels="BGR")
+    if "http" in source or "https" in source:
+        source = source.removeprefix("http://").removeprefix("https://").replace(".com","").replace("www.", "").replace("/", "_").replace(".", "_"). replace(":", "_")
+    elif source.isdigit():
+        source = f"cam{source}"
+    else:
+        source = source.split("/")[-1].split(".")[0]
+
+    return f"counts_{source}_{datetime.now().strftime("%d-%m-%y")}.csv"
 
 class MODEL:
     __model = None
     
-    def __init__(self) -> None:
+    def __init__(self, name: str = "Default") -> None:
+        self.name = name
+        self.stopped = False
         pass
 
     def __str__(self) -> str:
         if isinstance(self.__model, YOLO):
-            return "<class 'MODEL':YOLO>"
+            return f"<class 'MODEL':YOLO ({self.name})>"
         else:
             return "<class 'MODEL'>"
+        
+    def __repr__(self) -> str:
+        return self.__str__()
 
     def mount(self, model: any) -> None:
         self.__model = model
@@ -46,9 +60,10 @@ class MODEL:
         else:
             print("Operation unsupported for the mounted model.")
 
-    def __dump(self, counts: dict) -> None:
+    def __dump(self, counts: dict, source: str) -> None:
         classes = self.__model.names
-        with open("counts.csv", "w", newline="") as file:
+        filename = getName(source)
+        with open(filename, "w", newline="") as file:
             csv_writer = writer(file)
             csv_writer.writerow(["class", "counts"])
             for cls in list(classes.values()):
@@ -57,9 +72,9 @@ class MODEL:
                 else:
                     csv_writer.writerow([cls, 0])
 
-    def __clean_count(self, counts: dict) -> None:
+    def __clean_count(self, counts: dict, source:str) -> None:
         print(f"\nRAW = {counts}")
-        print("Object Counts:")
+        print(f"Object Counts for {getName(source):}")
         for cls, val in counts.items():
             if isinstance(val, dict):
                 keys = list(val.keys())
@@ -67,27 +82,24 @@ class MODEL:
                     for direction in keys:
                         print(f"{cls.capitalize()} ({direction}) = {val[direction]}")
 
-    def count(self, capture: VideoCapture = VideoCapture(0), skip: int = 1, region_points: list = [], resolution: tuple = (1280, 720), show_vid: bool = False) -> None:
+    def count(self, source: str = "0", skip: int = 1, region_points: list = [(500, 450), (500, 1300)], show_vid: bool = False) -> None:
         if isinstance(self.__model, YOLO):
             try:
-                frame_width, frame_height = resolution
                 frame_count = 0
-                # source = source if source != "0" else 0
-                # capture = VideoCapture(source)
-                capture.set(CAP_PROP_FRAME_WIDTH, frame_width)
-                capture.set(CAP_PROP_FRAME_HEIGHT, frame_height)
+                source = source if source != "0" else 0
+                capture = VideoCapture(source)
                 
                 object_counter = ObjectCounter(
                     view_img=show_vid,
                     reg_pts=region_points,
                     classes_names=self.__model.names,
-                    draw_tracks=True,
+                    draw_tracks=show_vid,
+                    view_in_counts=show_vid,
+                    view_out_counts=show_vid,
                     line_thickness=2
                 )
 
-                new_classwise_count = {}
                 old_classwise_count = {}
-
                 while capture.isOpened():
                     success, im0 = capture.read()
                     if not success:
@@ -98,21 +110,24 @@ class MODEL:
                     new_classwise_count = object_counter.class_wise_count
 
                     if new_classwise_count != old_classwise_count:
-                        self.__clean_count(new_classwise_count)
-                        self.__dump(new_classwise_count)
+                        self.__dump(new_classwise_count, source)
+                        # self.__clean_count(new_classwise_count, source)
                         old_classwise_count = deepcopy(new_classwise_count)
 
                     frame_count += skip
                     capture.set(CAP_PROP_POS_FRAMES, frame_count)
-                    if waitKey(1) == ord('q'):
+
+                    if self.stopped:
                         break
 
                 capture.release()
                 destroyAllWindows()
-                
             except Exception as e:
                 print(f"Error occurred: {e}")
         elif self.__model is None:
             print("No model detected. Mount a model first using `MODEL.mount(your_model)`")
         else:
             print("Operation unsupported for the mounted model.")
+
+    def stop(self):
+        self.stopped = True
